d15d3cea613baa904db0e7216e869da8f814676e
Ezequiel Di Giacomo <ezequiel250891@gmail.com>
2025-12-28 14:53:45 -0300

    url

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
-const API_URL = 'https://localhost:7112/api';
+// const API_URL = 'https://localhost:7112/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 const defaultHeaders = {
     'Content-Type': 'application/json',
